2b6b6e9a18151206edc9ef9dab2e04d181cafe04
Abdurashitov beksultan <beksultan1709@gmail.com>
2023-11-22 15:24:07 +0300

    first commit

---
+++ src/store/Data.js
@@ -0,0 +1,17 @@
+export const userList = [
+	{
+		name: 'Manas',
+		email: 'Manass1@gmail.com',
+		id: 1,
+	},
+	{
+		name: 'Tailak',
+		email: 'Tailak00@gmail.com',
+		id: 2,
+	},
+	{
+		name: 'Muhammed',
+		email: 'Muhammed001@gmail.com',
+		id: 3,
+	},
+]

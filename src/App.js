3d89bd6cc914e6121e59f4c53bc57348f46f6a51
Abdurashitov beksultan <beksultan1709@gmail.com>
2023-11-23 15:07:06 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react'
 import Home from './store/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Create from './store/Create';
-import Update from './store/Update';
+import Create from './store/Create'
+import Update from './store/Update'
 
 function App() {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
-				<Route path='/' element={<Home />}>
-					{' '}
-				</Route>
-				<Route path='/create' element={<Create />}>
-					{' '}
-				</Route>
-        <Route path='/edit/:id' element={<Update />}>
-					{' '}
-				</Route>
+				<Route path='*' element={<Home />}></Route>
+				<Route path='/create' element={<Create />}></Route>
+				<Route path='/edit/:id' element={<Update />}></Route>
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	)
 }
 

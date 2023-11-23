c7399f7fa17ace249274abc7c56ef787bb28b639
Abdurashitov beksultan <beksultan1709@gmail.com>
2023-11-23 15:15:08 +0300

    update App js Brouser router => hashrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from 'react'
-import Home from './store/Home'
-import { HashRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Create from './store/Create'
-import Update from './store/Update'
 
 function App() {
-	return (
-		<HashRouter>
-			<Routes>
-				<Route path='*' element={<Home />}></Route>
-				<Route path='/create' element={<Create />}></Route>
-				<Route path='/edit/:id' element={<Update />}></Route>
-			</Routes>
-		</HashRouter>
-	)
+	return (<></>)
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import UserReducer from './store/UserReducer'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from './store/Home'
+import Create from './store/Create'
+import Update from './store/Update'
 
 const store = configureStore({
 	reducer: {
@@ -15,6 +19,13 @@ root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<App />
+			<HashRouter>
+			<Routes>
+				<Route path='*' element={<Home />}></Route>
+				<Route path='/create' element={<Create />}></Route>
+				<Route path='/edit/:id' element={<Update />}></Route>
+			</Routes>
+		</HashRouter>
 		</Provider>
 	</React.StrictMode>,
 )
